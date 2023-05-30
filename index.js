b2cd7c5f3d1683653b23f58d9285630211bea9be
Kai Del Rosario <Maunakai123@yahoo.com>
2023-05-29 19:34:32 -0700

    COMPLETE LABBB

---
+++ index.js
@@ -0,0 +1,28 @@
+function add (a,b){
+    return (a + b);
+}
+function subtract (a,b){
+    return (a - b);
+}
+function multiply (a, b){
+    return (a * b);
+}
+function divide (a, b){
+    return (a / b);
+}
+function increment (n){
+    n++;
+    return n;
+}
+function decrement (n){
+    n--;
+    return n;
+}
+function makeInt (n){
+    const N = parseInt(n, 10);
+    return N;
+}
+function preserveDecimal (n){
+    const N = parseFloat(n);
+    return N;
+}
